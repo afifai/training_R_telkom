92abc3e963911a7ae0f635db49eddd1d07b2e937
afifai <afif.akbar@apecsa-indonesia.com>
2016-09-22 14:58:18 +0700

    Twitter feed

--- day_1/8.R
+++ day_1/8.R
@@ -1,10 +1,10 @@
 library(twitteR)
 
 
-api_key = "FkjcKfSd8DV6QR60FADlOPfeo"
-api_secret = "nlqVK5RKFmfSZBnqWl7TOHUaREIOGdeZWZejieHqN2VMum9jlW"
-access_token = "2346250518-duTu5wH3YxwAPKxY67vmysZB9wPDZlav1Pq38PA"
-access_token_secret = "U4wyPXsfawPtuaBEO43lo6KT3iYAHmWJPPZQCv2BXUszU"
+api_key = "API Key"
+api_secret = "API Secret"
+access_token = "Access Token"
+access_token_secret = "Access Token Secret"
 
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
